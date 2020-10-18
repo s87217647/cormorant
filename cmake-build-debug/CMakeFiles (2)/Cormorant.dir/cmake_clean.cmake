0b0255a4192ee9cb74fcbf1d1649be6c5f95464a
swiftsheng <s87217647@gmail.com>
2020-10-18 13:09:56 -0700

    Latest Update

---
+++ cmake-build-debug/CMakeFiles (2)/Cormorant.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cormorant.dir/src/main.cpp.obj"
+  "Cormorant.exe"
+  "Cormorant.exe.manifest"
+  "Cormorant.lib"
+  "Cormorant.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cormorant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
